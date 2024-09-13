18672940e9cd82d47c6c9012401b6617303bccf7
kodt2 <kodt2005@yandex.ru>
2024-09-13 09:39:05 +0300

    added non-logical methods

--- src/tbitfield.cpp
+++ src/tbitfield.cpp
@@ -9,46 +9,101 @@
 
 TBitField::TBitField(int len)
 {
+    if (len < 0) {
+        throw "Negative Length";
+    }
+    BitLen = len;//кол-во бит
+    MemLen = len / (sizeof(TELEM)*8);//кол-во переменных
+    if (len % (sizeof(TELEM)*8) != 0) {
+        MemLen++;
+    }
+    pMem = new TELEM[MemLen];
+    for (int i = 0; i < MemLen; i++) {
+        pMem[i] = 0;
+    }
 }
 
 TBitField::TBitField(const TBitField &bf) // конструктор копирования
 {
+    BitLen = bf.BitLen;
+    MemLen = bf.MemLen;
+    pMem = new TELEM[MemLen];
+    for (int i = 0; i < MemLen; i++) {
+        pMem[i] = bf.pMem[i];
+    }
 }
 
 TBitField::~TBitField()
 {
+    delete[] pMem;
 }
 
 int TBitField::GetMemIndex(const int n) const // индекс Мем для бита n
 {
+
     return 0;
 }
 
 TELEM TBitField::GetMemMask(const int n) const // битовая маска для бита n
 {
-    return TELEM();
+    TELEM res;
+    if (n < sizeof(TELEM) * 8) {
+        res = 1 << (sizeof(TELEM) * 8 - n - 1);
+    }
+    else {
+        res = 0;
+    }
+    return res;
 }
+
 
 // доступ к битам битового поля
 
 int TBitField::GetLength(void) const // получить длину (к-во битов)
 {
-  return 0;
+    return BitLen;
 }
 
 void TBitField::SetBit(const int n) // установить бит
 {
+    if (n < 0) {
+        throw "negative index";
+    }
+    if (n >= BitLen) {
+        throw "No such bit";
+    }
+    int Nperem = n / (sizeof(TELEM) * 8);
+    int Nbit = n % (sizeof(TELEM) * 8);
+    pMem[Nperem] = pMem[Nperem] | GetMemMask(Nbit);
 }
 
 void TBitField::ClrBit(const int n) // очистить бит
 {
+    if (n < 0) {
+        throw "negative index";
+    }
+    if (n >= BitLen) {
+        throw "No such bit";
+    }
+    int Nperem = n / (sizeof(TELEM) * 8);
+    int Nbit = n % (sizeof(TELEM) * 8);
+    pMem[Nperem] = pMem[Nperem] & ~GetMemMask(Nbit);
 }
 
 int TBitField::GetBit(const int n) const // получить значение бита
 {
-  return 0;
+    if (n < 0) {
+        throw "negative index";
+    }
+    if (n >= BitLen) {
+        throw "No such bit";
+    }
+    int Nperem = n / (sizeof(TELEM) * 8);
+    int Nbit = n % (sizeof(TELEM) * 8);
+    TELEM temp = pMem[Nperem];
+    temp = (temp & GetMemMask(n))>>((sizeof(TELEM) * 8) -n-1);
+    return temp;
 }
-
 // битовые операции
 
 TBitField& TBitField::operator=(const TBitField &bf) // присваивание
